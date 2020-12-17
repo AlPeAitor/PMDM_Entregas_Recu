32995688c273ec374868feb75ad7054ff339d478
AlPeAitor <61113259+AlPeAitor@users.noreply.github.com>
2020-12-17 12:33:01 +0100

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,4 @@
 import java.util.*
-import kotlin.random.*
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
@@ -8,7 +7,7 @@ fun main(args: Array<String>) {
 
     val sc = Scanner(System.`in`)
     while (!sc.hasNextInt()) {
-        System.out.println("Introduzca un número: ");
+        System.out.print("Introduzca un número: ");
         sc.next()
     }
     var numero = sc.nextInt()
@@ -16,15 +15,25 @@ fun main(args: Array<String>) {
     //Parte del cuadrado
     if (esPar(numero)){
         if(numero!=0) {
-            println(" _")
-            repeat(numero) {
-                println("| |")
+
+            repeat(numero){
+                print(" — ")
             }
-            println(" -")
+            println()
+            repeat(numero){
+                print("|")
+                repeat(numero){
+                    print("   ")   //parece que con 3 espacios se queda la forma del cuadrado mas consistente, comprobado con valores de 2 hasta 24
+                }
+                println("|")
+            }
+            repeat(numero) {
+                print(" — ")
+            }
         }
     }else {
 
-    //Parte de la lista
+        //Parte de la lista
 
         val lista = List(numero*5) { Random.nextInt(1,100) }
         print("Elementos: ")
@@ -42,24 +51,3 @@ fun main(args: Array<String>) {
 fun esPar (x: Int) : Boolean{
     return x%2==0
 }
-
-/*
-
-Crea un programa en Kotlin (Intellij) que:
-
-a) Pida al usuario que introduzca un número al que llamaremos X. (1 punto)
-b) Si el número es par, de mostrar un cuadrado de X elementos y mostrar los por pantalla del siguiente modo:
-Suponiendo X = 2 (3 puntos)
-
-
-
-c) Si el número es impar debe crearse una lista de X * 5 elementos compuesta de números aleatorios. (1 punto)
-
-Posteriormente debes:
-1- mostrar todos los elementos de la lista. (1 punto)
-2- mostrar los elementos pares de la lista. (2 puntos)
-3- mostrar los elementos ordenados. (2 puntos)
-
-
-
- */
